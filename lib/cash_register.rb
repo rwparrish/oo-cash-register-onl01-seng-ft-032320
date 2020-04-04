5d1c4b6d1af2c3cc48b55d76cb2934502425d012
Ryan Parrish <rino3717@gmail.com>
2020-04-04 18:41:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,10 @@ class CashRegister
   attr_accessor :items, :price, :discount, :total
   
 def initialize(discount = 0)
-  @total = 0
+  @total = 0 
+  @discount = discount
+  @total = []
+end
   
   
   
