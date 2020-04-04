da30812673fb0ac299ce5363763832337cb52e45
Ryan Parrish <rino3717@gmail.com>
2020-04-04 19:36:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,8 +15,11 @@ def add_item(item, price, quantity = 1)
 end
 
 def apply_discount
+  if @discount > 0
   @total -= (@total * @discount.to_f / 100)
   "After the discount, the total comes to $#{@total.to_i}."
+  else
+    
 end 
   
   
